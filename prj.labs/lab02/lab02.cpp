351180acd047e1df2e1a90dcd0da87fc4549aee3
DarkArtheme <artyom.bogdanov.2012@yandex.ru>
2022-02-19 19:26:24 +0300

    Initial setup before lab 2

--- prj.labs/lab02/lab02.cpp
+++ prj.labs/lab02/lab02.cpp
@@ -1,4 +1,13 @@
 #include <opencv2/opencv.hpp>
 
 int main() {
+	const std::string image_path = "./data/cross_0256x0256.png";
+	cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+	if (img.empty()) {
+		std::cout << "Could not read the image: " << image_path << std::endl;
+		return 1;
+	}
+	std::cout << "Image loaded" << std::endl;
+	cv::waitKey(0);
+	return 0;	
 }
